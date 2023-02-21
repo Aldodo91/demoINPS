87a929ada10a96192fe7ea53342fbc01d54bcac5
Aldodo91 <aldo.dorso@bcsoft.net>
2023-02-21 17:31:17 +0100

    dateConverter JSDOC

--- src/app/common/dateConverter.ts
+++ src/app/common/dateConverter.ts
@@ -1,5 +1,13 @@
 import { DatePipe } from '@angular/common';
 
+/**
+ * Remove the timestamp from a date.
+ *
+ * @param   {Date | undefined | null} [date]  The Date to convert.
+ * @param {string} [format=yyyy-MM-dd] - The format to convert into.
+ * @param {string} [locale=it-IT] - The locale to use in constructor.
+ * @returns {(string|undefined)} The value converted or undefined if it fails.
+ */
 export function dateConverter(
   date: Date | undefined | null,
   format: string = 'yyyy-MM-dd',
